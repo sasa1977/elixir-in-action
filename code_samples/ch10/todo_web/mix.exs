f2bcd63d89a0bec7ea919067cc012ffe8a1f2e1e
Sasa Juric <sasa.juric@gmail.com>
2015-01-24 14:32:13 +0100

    update plug to 0.10.0

--- code_samples/ch10/todo_web/mix.exs
+++ code_samples/ch10/todo_web/mix.exs
@@ -20,7 +20,7 @@ defmodule Todo.Mixfile do
     [
       {:gproc, "0.3.1"},
       {:cowboy, "1.0.0"},
-      {:plug, "0.8.1"},
+      {:plug, "0.10.0"},
       {:meck, "0.8.2", only: :test},
       {:httpoison, "0.4.3", only: :test}
     ]
