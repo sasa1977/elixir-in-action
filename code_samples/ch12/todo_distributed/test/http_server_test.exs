061dd60b36fd2eb85a3f825cc3300e0211477cfb
Sasa Juric <sasa.juric@gmail.com>
2016-01-05 20:42:36 +0100

    fix tests

--- code_samples/ch12/todo_distributed/test/http_server_test.exs
+++ code_samples/ch12/todo_distributed/test/http_server_test.exs
@@ -15,12 +15,12 @@ defmodule HttpServerTest do
 
   test "http server" do
     assert %HTTPoison.Response{body: "", status_code: 200} =
-      HTTPoison.get("http://127.0.0.1:5454/entries?list=test&date=20131219")
+      HTTPoison.get!("http://127.0.0.1:5454/entries?list=test&date=20131219")
 
     assert %HTTPoison.Response{body: "OK", status_code: 200} =
-      HTTPoison.post("http://127.0.0.1:5454/add_entry?list=test&date=20131219&title=Dentist", "")
+      HTTPoison.post!("http://127.0.0.1:5454/add_entry?list=test&date=20131219&title=Dentist", "")
 
     assert %HTTPoison.Response{body: "2013-12-19    Dentist", status_code: 200} =
-      HTTPoison.get("http://127.0.0.1:5454/entries?list=test&date=20131219")
+      HTTPoison.get!("http://127.0.0.1:5454/entries?list=test&date=20131219")
   end
 end
