138c490fa67ad304530b4498754edbfc8d3e4185
Sasa Juric <sasa.juric@gmail.com>
2017-12-24 16:04:38 +0100

    simplify todo-list operations

--- code_samples/ch04/todo_crud.ex
+++ code_samples/ch04/todo_crud.ex
@@ -3,18 +3,18 @@ defmodule TodoList do
 
   def new(), do: %TodoList{}
 
-  def add_entry(
-        %TodoList{entries: entries, auto_id: auto_id} = todo_list,
-        entry
-      ) do
-    entry = Map.put(entry, :id, auto_id)
-    new_entries = Map.put(entries, auto_id, entry)
+  def add_entry(todo_list, entry) do
+    entry = Map.put(entry, :id, todo_list.auto_id)
+    new_entries = Map.put(todo_list.entries, todo_list.auto_id, entry)
 
-    %TodoList{todo_list | entries: new_entries, auto_id: auto_id + 1}
+    %TodoList{todo_list |
+      entries: new_entries,
+      auto_id: todo_list.auto_id + 1
+    }
   end
 
-  def entries(%TodoList{entries: entries}, date) do
-    entries
+  def entries(todo_list, date) do
+    todo_list.entries
     |> Stream.filter(fn {_, entry} -> entry.date == date end)
     |> Enum.map(fn {_, entry} -> entry end)
   end
@@ -23,26 +23,19 @@ defmodule TodoList do
     update_entry(todo_list, new_entry.id, fn _ -> new_entry end)
   end
 
-  def update_entry(
-        %TodoList{entries: entries} = todo_list,
-        entry_id,
-        updater_fun
-      ) do
-    case entries[entry_id] do
-      nil ->
+  def update_entry(todo_list, entry_id, updater_fun) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
         todo_list
 
-      old_entry ->
+      {:ok, old_entry} ->
         new_entry = updater_fun.(old_entry)
-        new_entries = Map.put(entries, new_entry.id, new_entry)
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
         %TodoList{todo_list | entries: new_entries}
     end
   end
 
-  def delete_entry(
-        %TodoList{entries: entries} = todo_list,
-        entry_id
-      ) do
-    %TodoList{todo_list | entries: Map.delete(entries, entry_id)}
+  def delete_entry(todo_list, entry_id) do
+    %TodoList{todo_list | entries: Map.delete(todo_list.entries, entry_id)}
   end
 end
--- code_samples/ch06/server_process_todo.ex
+++ code_samples/ch06/server_process_todo.ex
@@ -82,55 +82,39 @@ defmodule TodoList do
     )
   end
 
-  def add_entry(
-    %TodoList{entries: entries, auto_id: auto_id} = todo_list,
-    entry
-  ) do
-    entry = Map.put(entry, :id, auto_id)
-    new_entries = Map.put(entries, auto_id, entry)
+  def add_entry(todo_list, entry) do
+    entry = Map.put(entry, :id, todo_list.auto_id)
+    new_entries = Map.put(todo_list.entries, todo_list.auto_id, entry)
 
     %TodoList{todo_list |
       entries: new_entries,
-      auto_id: auto_id + 1
+      auto_id: todo_list.auto_id + 1
     }
   end
 
-  def entries(%TodoList{entries: entries}, date) do
-    entries
-    |> Stream.filter(fn({_, entry}) ->
-         entry.date == date
-       end)
-
-    |> Enum.map(fn({_, entry}) ->
-         entry
-       end)
+  def entries(todo_list, date) do
+    todo_list.entries
+    |> Stream.filter(fn {_, entry} -> entry.date == date end)
+    |> Enum.map(fn {_, entry} -> entry end)
   end
-
 
   def update_entry(todo_list, %{} = new_entry) do
     update_entry(todo_list, new_entry.id, fn(_) -> new_entry end)
   end
 
-  def update_entry(
-    %TodoList{entries: entries} = todo_list,
-    entry_id,
-    updater_fun
-  ) do
-    case entries[entry_id] do
-      nil -> todo_list
+  def update_entry(todo_list, entry_id, updater_fun) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
 
-      old_entry ->
+      {:ok, old_entry} ->
         new_entry = updater_fun.(old_entry)
-        new_entries = Map.put(entries, new_entry.id, new_entry)
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
         %TodoList{todo_list | entries: new_entries}
     end
   end
 
-
-  def delete_entry(
-    %TodoList{entries: entries} = todo_list,
-    entry_id
-  ) do
-    %TodoList{todo_list | entries: Map.delete(entries, entry_id)}
+  def delete_entry(todo_list, entry_id) do
+    %TodoList{todo_list | entries: Map.delete(todo_list.entries, entry_id)}
   end
 end
