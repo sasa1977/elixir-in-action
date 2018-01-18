cfd940eb236fab437fa2ccc02759380d9f183cd2
Sasa Juric <sasa.juric@gmail.com>
2018-01-18 21:06:03 +0100

    use Elixir 1.6.0

--- code_samples/ch13/todo_release/mix.exs
+++ code_samples/ch13/todo_release/mix.exs
@@ -5,7 +5,7 @@ defmodule Todo.MixProject do
     [
       app: :todo,
       version: "0.1.0",
-      elixir: "~> 1.6-rc",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
