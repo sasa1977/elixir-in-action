96e621adde472ee52046e04aa430e14dba7f8f54
Egor Smorgunov <egorsmorgunov@gmail.com>
2021-03-19 15:08:39 +0300

    Update circle.ex

--- code_samples/ch02/circle.ex
+++ code_samples/ch02/circle.ex
@@ -3,8 +3,11 @@ defmodule Circle do
   @pi 3.14159
 
   @doc "Computes the area of a circle"
+  @spec area(number) :: number
   def area(r), do: r*r*@pi
 
   @doc "Computes the circumference of a circle"
+  @spec circumference(number) :: number
+  
   def circumference(r), do: 2*r*@pi
 end
