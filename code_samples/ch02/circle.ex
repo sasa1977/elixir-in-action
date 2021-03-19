19937fba2af1334509be396e2dbe0741e0f949d1
Egor Smorgunov <egorsmorgunov@gmail.com>
2021-03-19 13:28:43 +0300

    46page

---
+++ code_samples/ch02/circle.ex
@@ -0,0 +1,10 @@
+defmodule Circle do
+  @moduledoc "Implements basic circle functions"
+  @pi 3.14
+
+  @doc "Computes the area of a circle"
+  def area(r), do: r*r*@pi
+
+  @doc "Computes the circumference of a circle"
+  def circumference(r), do: 2*r*@pi
+end
